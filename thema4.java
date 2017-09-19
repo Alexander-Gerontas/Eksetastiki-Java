e1034c5aae3ec655a74be9a84b6d453b566b1f4a
Alexander <icsd15029@icsd.aegean.gr>
2017-09-20 00:10:39 +0300

    Create thema4.java

---
+++ thema4.java
@@ -0,0 +1,18 @@
+public class Main
+{
+    private static final int a1 = getc();
+    private static final int a2 = Integer.parseInt("10");
+    private static final int a3 = getc();
+
+    final public static int getc()
+    {
+        System.out.println(a2);
+        return a2;
+    }
+
+    public static void main(String[] args)
+    {
+//        Main m = new Main();
+        System.out.println(a1 + " " + a2 + " " + a3);
+    }
+}
